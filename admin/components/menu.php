749787235cd283329b193f1ad78b41675c3ff0e1
Aldhi <aldhi.albadri@gmail.com>
2023-12-17 17:44:22 +0700

    first commit

---
+++ admin/components/menu.php
@@ -0,0 +1,58 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active py-0" href="?page=dashboard">
+                    <i class="fa-solid fa-house fa-fw"></i>
+                    Dashboard <span class="sr-only">(current)</span>
+                </a>
+            </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Kategori</span>
+            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+            </a>
+        </h6>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link py-1" href="?page=kategori_data">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Data Kategori
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link py-1" href="?page=kategori_insert">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Kategori Baru
+                </a>
+            </li>
+
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Menu</span>
+            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+            </a>
+        </h6>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link py-1" href="?page=menu_data">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Data Menu
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link py-1" href="?page=menu_insert">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Menu Baru
+                </a>
+            </li>
+
+        </ul>
+
+    
+    </div>
+</nav>
