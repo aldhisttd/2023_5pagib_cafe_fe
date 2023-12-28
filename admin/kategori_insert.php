4a07a654f51cce6e24e0ffc225d42e724b49c73f
Acer V5 <resturamadan658@gmail.com>
2023-12-28 12:01:35 +0700

    fe-21/input_kategori

---
+++ admin/kategori_insert.php
@@ -0,0 +1,22 @@
+        
+                <div class="row my-4">
+                    <div class="col">
+                        <h2>Form Input Kategori</h2>
+                    </div>
+                </div>
+
+                <form id="myForm" class="form">
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Kode Kategori</label>
+                        <input type="text" class="form-control" name="kode_kategori" id="kode_kategori" placeholder="kode kategori">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Nama Kategori</label>
+                        <input type="text" class="form-control" name="nama_kategori" id="nama_kategori" placeholder="nama kategori">
+                    </div>
+                    <button type="submit" class=" btn btn-primary" id="submitBtn">simpan</button>
+            </div>
+        </div>
+        </form>
+    </div>
+  
---
+++ admin/script/kategori_insert.js
@@ -0,0 +1,24 @@
+const host =
+    'http://localhost' +
+    window.location.pathname.replace('kategori_insert.php', '');
+
+$(document).ready(function () {
+    $('#myForm').submit(function (event) {
+        event.preventDefault();
+
+        var formData = $('#myForm').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: host + 'kategori_create.php',
+            data: formData,
+            success: function (response) {
+                console.log(response);
+                alert('Data berhasil ditambahkan');
+            },
+            error: function (error) {
+                console.error(error);
+            },
+        });
+    });
+});
