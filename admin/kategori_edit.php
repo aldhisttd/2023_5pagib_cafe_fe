e50fd1b0bceb2b7989158edbd42825df5d4ba35d
putriEkaOktaPiya <putriekaoktapiya@gmail.com>
2024-01-03 23:30:39 +0700

    22_edit_kategori

---
+++ admin/kategori_edit.php
@@ -0,0 +1,26 @@
+<main role="main" class="col-lg-7 px-md-2">
+    <!-- title -->
+    <div class="row mt-3">
+        <div class="col">
+            <h3>Menu Update</h3>
+        </div>
+    </div>
+    <!-- form -->
+    <div class="row mt-2">
+        <div class="col">
+            <form id="kategoriUpdate" action="">
+                <div class="form-group">
+                    <label for="kode">Kode</label>
+                    <input type="text" class="form-control" name="kode" id="kode" readonly/>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" name="nama" id="nama"/>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-lg" type="submit">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>  
+</main>
