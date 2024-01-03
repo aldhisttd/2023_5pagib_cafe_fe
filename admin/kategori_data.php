1dd6739ce0d191923c4bc1c97e0381de4b57363a
sitiannisaa <sitiannisamaulidya21@gmail.com>
2024-01-03 23:42:11 +0700

    23_data_kategori revisi

--- admin/kategori_data.php
+++ admin/kategori_data.php
@@ -1,22 +1,24 @@
-<div class="row mt-3">
-    <div class="col">
-        <h3>Kategori Data</h3>
+<div class="col">
+    <!-- konten -->
+    <div class="row my-4">
+        <div class="col">
+            <h2>Tabel Kategori</h2>
+        </div>
     </div>
-</div>
-
     <div class="row">
         <div class="col">
-            <table class="table table-bordered">
+            <table class="table">
                 <thead>
                     <tr>
                         <th scope="col">No</th>
                         <th scope="col">Kode</th>
-                        <th scope="col">Nama Kategori</th>
+                        <th scope="col">Nama</th>
                         <th scope="col">Aksi</th>
                     </tr>
                 </thead>
-        
-            <tbody id="dataKategori"> </tbody>
+                <tbody id="kategoriData">
+                </tbody>
             </table>
         </div>
     </div>
+</div>
--- admin/script/kategori_data.js
+++ admin/script/kategori_data.js
@@ -1,83 +1,58 @@
 $(document).ready(function () {
-    // Fungsi untuk memuat data kategori
-    function loadKategoriData() {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost/2023_5pagib_cafe_fe/dummy/kategori_read.php",
-            dataType: "json",
-            success: function (response) {
-                var dataKategori = response.body.data;
+  readKategori();
 
-                // Hapus data yang ada
-                $("#dataKategori").empty();
+  // API read
+  function readKategori() {
+    $("#kategoriData").empty();
+    $.ajax({
+      type: "GET",
+      url: host + "kategori_read.php",
+      dataType: "json",
+      async: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function (data) {
+        var dataKategori = data.body.data;
+        for (var i = 0; i < dataKategori.length; i++) {
+          $("#kategoriData").append(
+            "<tr>" +
+              "<td>" + (i + 1) + "</td>" +
+              "<td>" + dataKategori[i].kode + "</td>" +
+              "<td>" + dataKategori[i].nama + "</td>" +
+              "<td>" +
+              "<button class='btn btn-danger hapus' value='" +
+              dataKategori[i].kode +
+              "'>Hapus</button>" +
+              "<button class='btn btn-primary'>" +
+              "<a class='text-light' href='?page=kategori_edit&kode=" +
+              dataKategori[i].kode +
+              "'>Edit</a>" +
+              "</button>" +
+              "</td>" +
+              "</tr>"
+          );
+        }
+      },
+    });
+  }
 
-                // Muat ulang data kategori setelah penghapusan berhasil
-                for (var i = 0; i < dataKategori.length; i++) {
-                    $("#dataKategori").append(
-                        `
-                        <tr>
-                            <td>` +
-                        (i + 1) +
-                        `</td>
-                            <td>` +
-                        dataKategori[i].kode +
-                        `</td>
-                            <td>` +
-                        dataKategori[i].nama_kategori +
-                        `</td>
-                            <td>
-                                <button class="btn btn-danger delete-btn" data-kode="` +
-                        dataKategori[i].kode +
-                        `">Hapus</button>
-                                <button class="btn btn-primary edit-btn">
-                                    <a class="text-light" href="./?page=kategori_edit?kode=` +
-                        dataKategori[i].kode +
-                        `">Edit</a>
-                                </button>
-                            </td>
-                        </tr>
-                        `
-                    );
-                }
+  // API hapus
+  $(document).on("click", ".hapus", function () {
+    var kode = $(this).val();
 
-                // Reset berhasil, berikan alert tambahan
-                alert("Data kategori berhasil diambil");
-            },
-            error: function (xhr, status, error) {
-                console.error("AJAX Error: " + status, error);
-            }
-        });
+    if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
+      $.ajax({
+        type: "POST",
+        url: host + "kategori_delete.php",
+        data: { kode: kode },
+        dataType: "json",
+        async: true,
+        success: function (response) {
+          alert(response.msg);
+          readKategori();
+        },
+      });
     }
-
-    // Fungsi untuk menangani klik tombol hapus
-    function handleDeleteButtonClick() {
-        $(document).on("click", ".delete-btn", function () {
-            var kode = $(this).data("kode");
-
-            if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
-                $.ajax({
-                    type: "POST",
-                    url: "http://localhost/2023_5pagib_cafe_fe/dummy/kategori_delete.php",
-                    data: { kode: kode },
-                    dataType: "json",
-                    success: function (response) {
-                        alert(response.msg);
-                        if (response.status === 200) {
-                            // Muat ulang data kategori setelah penghapusan berhasil
-                            loadKategoriData();
-                        }
-                    },
-                    error: function (xhr, status, error) {
-                        console.error("AJAX Error: " + status, error);
-                    }
-                });
-            }
-        });
-    }
-
-    // Panggil fungsi loadKategoriData saat memuat halaman
-    loadKategoriData();
-
-    // Panggil fungsi handleDeleteButtonClick untuk menangani klik tombol hapus
-    handleDeleteButtonClick();
+  });
 });
