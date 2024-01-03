d99926a407aec2b6c3ce4a67ace75c43c7501172
sitijamila32 <ilaee32@gmail.com>
2024-01-04 01:31:31 +0700

    26_data_delete_menu

---
+++ admin/menu_data.php
@@ -0,0 +1,29 @@
+<div class="col">
+        <!-- konten -->
+        <div class="row my-4">
+            <div class="col">
+                <h2>Tabel Menu</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Kode</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Kode Kategori</th>
+                            <th scope="col">Nama Kategori</th>
+                            <th scope="col">Gambar</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="menuTable">
+                        <!-- Data menu akan dimasukkan secara dinamis di sini menggunakan JavaScript -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+</div>
