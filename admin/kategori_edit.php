845d0b37f91566e4560effc17d4ef10e27e56974
putriEkaOktaPiya <putriekaoktapiya@gmail.com>
2024-01-05 03:00:42 +0700

    22_edit_kategori

--- admin/kategori_edit.php
+++ admin/kategori_edit.php
@@ -1,26 +1,21 @@
-<main role="main" class="col-lg-7 px-md-2">
-    <!-- title -->
-    <div class="row mt-3">
-        <div class="col">
-            <h3>Menu Update</h3>
-        </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Menu Update</h1>
+</div>
+
+<div class="row">
+    <div class="col">
+        <form id="kategoriUpdate">
+            <div class="form-group">
+                <label for="kode">Kode</label>
+                <input type="text" class="form-control" name="kode" id="kode" readonly/>
+            </div>
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" name="nama" id="nama"/>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary btn-lg" type="submit">Update</button>
+            </div>
+        </form>    
     </div>
-    <!-- form -->
-    <div class="row mt-2">
-        <div class="col">
-            <form id="kategoriUpdate">
-                <div class="form-group">
-                    <label for="kode">Kode</label>
-                    <input type="text" class="form-control" name="kode" id="kode" readonly/>
-                </div>
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" name="nama" id="nama"/>
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary btn-lg" type="submit">Update</button>
-                </div>
-            </form>
-        </div>
-    </div>  
-</main>
+</div>
