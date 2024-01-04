e4e96ebaac2d734123d84208f00e7d3ead426669
sitijamila32 <ilaee32@gmail.com>
2024-01-05 02:38:19 +0700

    26_data_delete_menu

--- admin/menu_data.php
+++ admin/menu_data.php
@@ -1,29 +1,26 @@
-<div class="col">
-        <!-- konten -->
-        <div class="row my-4">
-            <div class="col">
-                <h2>Tabel Menu</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Kode</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Kode Kategori</th>
-                            <th scope="col">Nama Kategori</th>
-                            <th scope="col">Gambar</th>
-                            <th scope="col">Harga</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody id="menuTable">
-                        <!-- Data menu akan dimasukkan secara dinamis di sini menggunakan JavaScript -->
-                    </tbody>
-                </table>
-            </div>
-        </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Tabel Menu</h1>
 </div>
+
+<div class="row">
+    <div class="col">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Kode</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Kode Kategori</th>
+                    <th scope="col">Nama Kategori</th>
+                    <th scope="col">Gambar</th>
+                    <th scope="col">Harga</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody id="menuTable">
+            <!-- Data menu akan dimasukkan secara dinamis di sini menggunakan JavaScript -->
+            </tbody>
+        </table>
+    </div>
+</div>
+
--- admin/script/menu_data.js
+++ admin/script/menu_data.js
@@ -17,40 +17,18 @@ $(document).ready(function () {
                     $("#menuTable").append(
                         `
                     <tr>
-                        <td>` +
-                            (i + 1) +
-                            `</td>
-                        <td>` +
-                            dataMenu[i].kode +
-                            `</td>
-                        <td>` +
-                            dataMenu[i].nama +
-                            `</td>
-                        <td>` +
-                            dataMenu[i].kode_kategori +
-                            `</td>
-                        <td>` +
-                            dataMenu[i].nama_kategori +
-                            `</td>
-                        <td>
-                            <img src="` +
-                            host +
-                            dataMenu[i].gambar +
-                            `" width="100" />
-                        </td>
-                        <td>` +
-                            dataMenu[i].harga +
-                            `</td>
-                        <td>
-                        <button id="hapus" class="btn btn-danger" value="` +
-                            dataMenu[i].kode +
-                            `">Hapus
-                        </button>
+                        <td>` + (i + 1) + `</td>
+                        <td>` + dataMenu[i].kode + `</td>
+                        <td>` + dataMenu[i].nama + `</td>
+                        <td>` + dataMenu[i].kode_kategori + `</td>
+                        <td>` + dataMenu[i].nama_kategori + `</td>
+                        <td> <img src="` + host + dataMenu[i].gambar + `" width="100" /> </td>
+                        <td>` + dataMenu[i].harga + `</td>
+                        <td> 
+                        <button id="hapus" class="btn btn-danger" value="` + dataMenu[i].kode + `">Hapus </button>
                         <button id="edit" class="btn btn-primary">
-                                <a class="text-light" href="?page=menu_edit&kode=` +
-                            dataMenu[i].kode +
-                            `">Edit</a>
-                            </button>
+                            <a class="text-light" href="?page=menu_edit&kode=` + dataMenu[i].kode + `">Edit</a>
+                        </button>
                         </td>
                     </tr>
                     `
@@ -59,9 +37,8 @@ $(document).ready(function () {
             },
         });
     }
-
     // api hapus
-    $(document).on("click", "#hapus", function () {
+$(document).on("click", "#hapus", function () {
         var kode = $(this).val();
 
         if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
