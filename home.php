9e9248b003aa48fcc882551f52f9f1c1a7d576cc
Denihendriawan123 <resturamadan658@gmail.com>
2024-01-19 20:01:46 +0700

    39-Web-Public-Dashboard

---
+++ home.php
@@ -0,0 +1,26 @@
+<main role="main"> 
+  <section class="jumbotron text-center"> 
+    <div class="container"> 
+      <h1>Cafe 5b</h1> 
+      <br> 
+      <p>Cafe 5b menyediakan makanan, minuman, dan cemilan. <br> Semua makanan dan minuman yang ada di cafe ini terjamin kebersihannya dan kelezatannya, menggunakan bahan bahan yang berkualitas</p> 
+    </div> 
+  </section> 
+</main>
+<div class="album"> 
+  <div class="container"> 
+    <div class="row" id="dataMenu"> 
+      <!-- Kartu-kartu Anda tetap ada di sini -->
+    </div>
+    
+
+    <!-- Tombol Previous dan Next -->
+    <div class="btn-group" id="btnGroup">
+      <button type="button" class="btn btn-primary btn-sm" id="prevBtn">Previous</button>
+      <div class="pagination"></div> <!-- Tambah elemen untuk nomor halaman -->
+      <button type="button" class="btn btn-primary btn-sm" id="nextBtn">Next</button>
+    </div>
+
+  </div> 
+</div>
+
